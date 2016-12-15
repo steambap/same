7c1e9ba6395de7a76acfc0a847115e4f5b7b6235
Weilin Shi <934587911@qq.com>
2016-12-15 20:30:44 +0800

    can destroy tiles now

--- in-game.js
+++ in-game.js
@@ -1,4 +1,5 @@
 const opts = require('./constants');
+const Point = Phaser.Point;
 
 class InGame {
 	constructor() {
@@ -44,8 +45,8 @@ class InGame {
 		this.tilesArray[row][col] = {
 			tileSprite: tile,
 			isEmpty: false,
-			coordinate: new Phaser.Point(col, row),
-			value: tile.tint
+			coordinate: new Point(row, col),
+			tint: tile.tint
 		};
 
 		this.tileGroup.add(tile);
@@ -69,7 +70,75 @@ class InGame {
 	}
 
 	pickTile(e) {
-		
+		if (this.canPick) {
+			const posX = e.x - this.tileGroup.x;
+			const posY = e.y - this.tileGroup.y;
+
+			const pickedRow = Math.floor(posY / opts.tileSize);
+			const pickedCol = Math.floor(posX / opts.tileSize);
+
+			if (this.isValidTile(pickedRow, pickedCol)) {
+				const pickedTile = this.tilesArray[pickedRow][pickedCol];
+
+				return this.tryPick(pickedTile);
+			}
+		}
+	}
+
+	isValidTile(row, col) {
+		return row >= 0 && col >=0 &&
+			row < opts.rows && col < opts.cols;
+	}
+
+	tryPick(theTile) {
+		const fill = [];
+		this.floodFill(theTile.coordinate, theTile.tint, fill);
+
+		if (fill.length > 2) {
+			this.destroyTiles(fill);
+		}
+	}
+
+	destroyTiles(tileList) {
+		tileList.forEach(point => {
+			const tile = this.tilesArray[point.x][point.y];
+			const tween = this.game.add.tween(tile.tileSprite).to({
+				alpha: 0
+			}, 300, Phaser.Easing.Linear.None, true);
+
+			this.tilePool.push(tile.tileSprite);
+			tween.onComplete.add(e => {
+				// fill vertical holes
+			});
+
+			tile.isEmpty = true;
+		});
+	}
+
+	// find same color tile
+	floodFill(point, color, fillArr) {
+		const {x, y} = point;
+		if (!this.isValidTile(x, y)) {
+			return;
+		}
+		if (this.tilesArray[x][y].isEmpty) {
+			return;
+		}
+		if (this.pointInArr(fillArr, point)) {
+			return;
+		}
+		if (this.tilesArray[x][y].tint === color) {
+			fillArr.push(point);
+			this.floodFill(new Point(x + 1, y), color, fillArr);
+			this.floodFill(new Point(x - 1, y), color, fillArr);
+			this.floodFill(new Point(x, y + 1), color, fillArr);
+			this.floodFill(new Point(x, y - 1), color, fillArr);
+		}
+	}
+
+	// check for phaser point in array
+	pointInArr(arr, point) {
+		return arr.some(myPoint => myPoint.equals(point));
 	}
 }
 
